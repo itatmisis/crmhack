fa659da0f1d9f802b504ff46cc9379a24f04b717
root <root@Lorentz.selectel.ru>
2021-08-21 14:14:35 +0300

    fix: minor fin in downloader

--- backend/app/vosk/download.sh
+++ backend/app/vosk/download.sh
@@ -4,4 +4,4 @@ cd /tmp
 wget https://alphacephei.com/vosk/models/vosk-model-small-ru-0.15.zip
 unzip vosk-model-small-ru-0.15.zip
 cd -
-mv /tmp/vosk-model-small-ru-0.15/* ./*
+mv /tmp/vosk-model-small-ru-0.15/* ./
