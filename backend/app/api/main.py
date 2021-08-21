1137188c09558b1a6606f1cbb4eaa41670166e3a
root <root@Lorentz.selectel.ru>
2021-08-21 15:54:28 +0300

    update

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, Request, File, UploadFile, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from loguru import logger
 
 from ..core.settings import Settings
 
 app = FastAPI(title=Settings().project_name, version=Settings().version, description=Settings().fast_api_description)
+# FIX_ME: менять в проде allow_origins на локальные!
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # Заглушка на /
--- backend/app/vosk/download.sh
+++ backend/app/vosk/download.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd /tmp
+#wget https://alphacephei.com/vosk/models/vosk-model-ru-0.10.zip # Версия на 2.5ГБ, качественее.
 wget https://alphacephei.com/vosk/models/vosk-model-small-ru-0.15.zip
 unzip vosk-model-small-ru-0.15.zip
 cd -
