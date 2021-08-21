df261c593655d7aa18de965ac5f5898de33c0790
root <root@Lorentz.selectel.ru>
2021-08-22 00:48:48 +0300

    frontend something

--- backend/app/data/vosk/download.sh
+++ backend/app/data/vosk/download.sh
@@ -4,11 +4,11 @@ cp download.sh /tmp
 rm -rf ./*
 mv /tmp/download.sh ./
 cd /tmp
-#wget https://alphacephei.com/vosk/models/vosk-model-ru-0.10.zip # Версия на 2.5ГБ, качественее.
-#unzip /tmp/vosk-model-ru-0.10.zip
-#cd -
-#mv /tmp/vosk-model-ru-0.10/* ./
-wget https://alphacephei.com/vosk/models/vosk-model-small-ru-0.15.zip
-unzip vosk-model-small-ru-0.15.zip
+wget https://alphacephei.com/vosk/models/vosk-model-ru-0.10.zip # Версия на 2.5ГБ, качественее.
+unzip /tmp/vosk-model-ru-0.10.zip
 cd -
-mv /tmp/vosk-model-small-ru-0.15/* ./
+mv /tmp/vosk-model-ru-0.10/* ./
+#wget https://alphacephei.com/vosk/models/vosk-model-small-ru-0.15.zip
+#unzip vosk-model-small-ru-0.15.zip
+#cd -
+#mv /tmp/vosk-model-small-ru-0.15/* ./
