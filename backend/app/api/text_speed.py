f3de58378ff8160798bde7016dc01eef9c1fcce8
root <root@Lorentz.selectel.ru>
2021-08-22 11:11:04 +0300

    fix

--- backend/app/api/text_speed.py
+++ backend/app/api/text_speed.py
@@ -4,17 +4,17 @@ import numpy as np
 def average_speed(data: dict):
     syllables = []
     if "result" not in data:
-        return 0
+        return {"mean": 0, "speed_class": "Нет данных"}
     for word in data["result"]:
         vowels = sum(x in 'aeiouауоыэяюёие' for x in word['word']) + 1
         time = word['end'] - word['start']
         for i in range(vowels):
-            syllables.append(time / vowels)
+            syllables.append(vowels / time)
     mean = np.array(syllables).mean()
     class_ = ""
-    if mean < 2.3:
+    if mean < 8.5:
         class_ = "Слишком медленно"
-    elif mean < 5:
+    elif mean < 11.5:
         class_ = "Нормально"
     else:
         class_ = "Слишком быстро"
